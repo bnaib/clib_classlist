75ca768d8e15034ce085009d1070beae6369783a
ice4x <ice4x@mail.ru>
2020-06-11 15:53:56 +0300

    Fix cut + add del

--- inc/class_list.h
+++ inc/class_list.h
@@ -3,7 +3,7 @@
      /*    By: Sergey Nikolaev                      / ___// | / /          */
     /*    Сontacts: sn.prog@yandex.ru               \__ \/  |/ /          */
    /*    Created: 2020.05.07 (YYYY.MM.DD)          ___/ / /|  /          */
-  /*    Updated: 2020.05.07 (YYYY.MM.DD)          /____/_/ |_/          */
+  /*    Updated: 2020.06.11 (YYYY.MM.DD)          /____/_/ |_/          */
  /*                                                                    */
 /* ****************************************************************** */
 
@@ -21,7 +21,7 @@ typedef struct					s_list_element
 
 typedef struct					s_class_list
 {
-	void						(*free_list_content)(void *);
+	void						(*free_list_content)(void *);      
 	size_t						length;
 	size_t						content_size;
 	t_list_element				*p_element;
@@ -32,12 +32,24 @@ t_class_list					*class_list_construct(
 	void (*free_list_content)(void *));
 void							class_list_destruct(
 	t_class_list **pp_list);
+
 void							class_list_add_to_end(
 	t_class_list *p_list,
 	t_list_element *p_element);
 void							class_list_add_to_start(
 	t_class_list *p_list,
 	t_list_element *p_element);
+
+t_list_element					*class_list_create_element(
+	const t_class_list *p_list,
+	const void *p_content);
+t_list_element					*class_list_create_element_to_end(
+	t_class_list *p_list,
+	const void *p_content);
+t_list_element					*class_list_create_element_to_start(
+	t_class_list *p_list,
+	const void *p_content);
+
 t_list_element					*class_list_get_root(
 	const t_class_list *p_list);
 t_list_element					*class_list_get_end(
@@ -48,9 +60,10 @@ t_list_element					*class_list_get_element_from_start_index(
 t_list_element					*class_list_get_element_from_end_index(
 	const t_class_list *p_list,
 	size_t index);
-t_list_element					*class_list_create_element(
-	const t_class_list *p_list,
-	const void *p_content);
+
+void							class_list_cut_element(
+	t_class_list *p_list,
+	t_list_element *p_element);
 t_list_element					*class_list_cut_root(
 	t_class_list *p_list);
 t_list_element					*class_list_cut_end(
@@ -59,6 +72,20 @@ t_list_element					*class_list_cut_element_from_start_index(
 	t_class_list *p_list,
 	size_t index);
 t_list_element					*class_list_cut_element_from_end_index(
+	t_class_list *p_list,
+	size_t index);
+
+void							class_list_del_element(
+	t_class_list *p_list,
+	t_list_element **pp_element);
+void				class_list_del_root(
+	t_class_list *p_list);
+void				class_list_del_end(
+	t_class_list *p_list);
+void				class_list_del_element_from_start_index(
+	t_class_list *p_list,
+	size_t index);
+void				class_list_del_element_from_end_index(
 	t_class_list *p_list,
 	size_t index);
 #endif
--- src/class_list_del.c
+++ src/class_list_del.c
@@ -2,75 +2,62 @@
       /*                                             _____ _   __           */
      /*    By: Sergey Nikolaev                      / ___// | / /          */
     /*    Сontacts: sn.prog@yandex.ru               \__ \/  |/ /          */
-   /*    Created: 2020.05.07 (YYYY.MM.DD)          ___/ / /|  /          */
+   /*    Created: 2020.06.11 (YYYY.MM.DD)          ___/ / /|  /          */
   /*    Updated: 2020.06.11 (YYYY.MM.DD)          /____/_/ |_/          */
  /*                                                                    */
 /* ****************************************************************** */
 
 #include "class_list.h"
 
-void						class_list_cut_element(
+void				class_list_del_element(
 	t_class_list *p_list,
-	t_list_element *p_element)
+	t_list_element **pp_element)
 {
-	if (p_element == NULL)
-		return ;
-	p_list->length--;
-	if (p_list->length == 0)
-		p_list->p_element = NULL;
-	else
-	{
-		if (p_element == p_list->p_element)
-			p_list->p_element = p_list->p_element->p_next;
-		p_element->p_next->p_last = p_element->p_last;
-		p_element->p_last->p_next = p_element->p_next;
-		p_element->p_next = NULL;
-		p_element->p_last = NULL;
-	}
+	p_list->free_list_content(*pp_element);
+	if ((*pp_element)->p_next != NULL && (*pp_element)->p_last != NULL)
+		class_list_cut_element(p_list, *pp_element);
+	free(*pp_element);
+	*pp_element = NULL;
 }
 
-t_list_element				*class_list_cut_root(
+void				class_list_del_root(
 	t_class_list *p_list)
 {
 	t_list_element	*p_element;
 	
 	if ((p_element = class_list_get_root(p_list)) == NULL)
-		return (NULL);
-	class_list_cut_element(p_list, p_element);
-	return (p_element);
+		return ;
+	class_list_del_element(p_list, &p_element);
 }
 
-t_list_element				*class_list_cut_end(
+void				class_list_del_end(
 	t_class_list *p_list)
 {
 	t_list_element	*p_element;
 	
 	if ((p_element = class_list_get_end(p_list)) == NULL)
-		return (NULL);
-	class_list_cut_element(p_list, p_element);
-	return (p_element);
+		return ;
+	class_list_del_element(p_list, &p_element);
 }
 
-t_list_element				*class_list_cut_element_from_start_index(
+void				class_list_del_element_from_start_index(
 	t_class_list *p_list,
 	size_t index)
 {
 	t_list_element	*p_element;
 	
 	if ((p_element = class_list_get_element_from_start_index(p_list, index)) == NULL)
-		return (NULL);
-	class_list_cut_element(p_list, p_element);
-	return (p_element);
+		return ;
+	class_list_del_element(p_list, &p_element);
 }
 
-t_list_element				*class_list_cut_element_from_end_index(
+void				class_list_del_element_from_end_index(
 	t_class_list *p_list,
 	size_t index)
 {
 	t_list_element	*p_element;
 	
 	if ((p_element = class_list_get_element_from_end_index(p_list, index)) == NULL)
-		return (NULL);
-	class_list_cut_element(p_list, p_element);
-	return (p_element);
+		return ;
+	class_list_del_element(p_list, &p_element);
 }
